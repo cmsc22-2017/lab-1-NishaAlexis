ee9cf413e884fedf2932cb50a135bd4c6c1073ea
NishaAlexis <31622277+NishaAlexis@users.noreply.github.com>
2017-09-07 20:53:04 +0800

    modified

--- IDeliMenu.java
+++ IDeliMenu.java
@@ -1,22 +1,26 @@
 interface IDeliMenu {}
 
 class Soup implements IDeliMenu {
-	NamePrice soup;
+	String name;
+	int price;
 	boolean isVegetarian;
 
-	Soup(NamePrice soup, boolean isVegetarian){
-		this.soup = soup;
+	Soup(String name, int price, boolean isVegetarian){
+		this.name = name;
+		this.price = price;
 		this.isVegetarian = isVegetarian;
 	}
 }
 
 class Salad implements IDeliMenu {
-	NamePrice salad;
+	String name;
+	int price;
 	boolean isVegetarian;
 	String dressing;
 
-	Salad(NamePrice salad, boolean isVegetarian, String dressing){
-		this.salad = salad;
+	Salad(String name, int price, boolean isVegetarian, String dressing){
+		this.name = name;
+		this.price = price;
 		this.isVegetarian = isVegetarian;
 		this.dressing =dressing;
 	}
@@ -24,44 +28,28 @@ class Salad implements IDeliMenu {
 }
 
 class Sandwich implements IDeliMenu {
-	NamePrice sandwich;
+	String name;
+	int price;
 	String breadType;
 	String filling1;
 	String filling2;
 
-	Sandwich(NamePrice sandwich, String breadType, String filling1, String filling2){
-		this.sandwich = sandwich;
+	Sandwich(String name, int price, String breadType, String filling1, String filling2){
+		this.name = name;
+		this.price = price;
 		this.breadType = breadType;
 		this.filling1 = filling1;
 		this.filling2 = filling2;
 	}
 }
 
-class NamePrice { // TODO Remove this class, I think it's unecessary (we'll discuss a better solution soon)
-	String name;
-	int price;
-
-	NamePrice(String name, int price){
-		this.name = name;
-		this.price = price;
-	}
-}
-
 class ExamplesMenu {
 	ExamplesMenu() {}
 
-	NamePrice a = new NamePrice("Mushroom Soup", 30);
-	NamePrice b = new NamePrice("Chicken Soup", 50);
-	NamePrice c = new NamePrice("Ceasar Salad", 40);
-	NamePrice d = new NamePrice("All Veggies Salad", 60);
-	NamePrice e = new NamePrice("PBJ", 50);
-	NamePrice f = new NamePrice("HamandCheese", 30);
-
-	IDeliMenu q = new Soup(a, true);
-	IDeliMenu r = new Soup(b, false);
-	IDeliMenu s = new Salad(c, false, "Ceasar");
-	IDeliMenu t = new Salad(d, true, "Thousand Island");
-	IDeliMenu u = new Sandwich(e, "Plain", "Peanut Butter", "Jelly");
-	IDeliMenu v = new Sandwich(f, "Wheat", "Ham", "Cheese");
-
+	IDeliMenu q = new Soup("Mushroom Soup", 30, true);
+	IDeliMenu r = new Soup("Chicken Soup", 50, false);
+	IDeliMenu s = new Salad("Ceasar Salad", 40, false, "Ceasar");
+	IDeliMenu t = new Salad("All Veggies Salad", 60, true, "Thousand Island");
+	IDeliMenu u = new Sandwich("PBJ", 50, "Plain", "Peanut Butter", "Jelly");
+	IDeliMenu v = new Sandwich("HamandCheese", 30, "Wheat", "Ham", "Cheese");
 }
